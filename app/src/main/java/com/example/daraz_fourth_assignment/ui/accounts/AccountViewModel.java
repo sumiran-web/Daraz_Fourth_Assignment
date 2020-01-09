ae5b19e9f4cfa43b0338e084937c8a506f367c19
sumiran <giri.sumiran111@gmail.com>
2020-01-09 13:28:38 +0545

    account view model fragment added

--- app/src/main/java/com/example/daraz_fourth_assignment/ui/accounts/AccountViewModel.java
+++ app/src/main/java/com/example/daraz_fourth_assignment/ui/accounts/AccountViewModel.java
@@ -1,6 +1,19 @@
 package com.example.daraz_fourth_assignment.ui.accounts;
 
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
 
-public class AccountViewModel extends ViewModel {
+public class AccountViewModel extends ViewModel  {
+    private MutableLiveData<String> mText;
+
+    public AccountViewModel() {
+        mText = new MutableLiveData<>();
+        mText.setValue("This is Account fragment");
+    }
+
+    public LiveData<String> getText() {
+        return mText;
+    }
 }
+
