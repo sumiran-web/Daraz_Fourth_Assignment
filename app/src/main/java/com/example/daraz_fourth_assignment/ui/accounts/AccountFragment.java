2d596e67fb4d48c98ce2b81dea3f0decd8fe1370
sumiran <giri.sumiran111@gmail.com>
2020-01-09 13:24:57 +0545

    account fragment created

---
+++ app/src/main/java/com/example/daraz_fourth_assignment/ui/accounts/AccountFragment.java
@@ -0,0 +1,32 @@
+package com.example.daraz_fourth_assignment.ui.accounts;
+
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.daraz_fourth_assignment.R;
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class AccountFragment extends Fragment {
+
+
+    public AccountFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_account, container, false);
+    }
+
+}
